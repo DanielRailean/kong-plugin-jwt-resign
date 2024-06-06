8f7efcbc556192ecef4af35fe7f0311ee68545c9
DanielRailean <daniel.railean1@gmail.com>
2024-06-06 23:05:08 +0200

    somewhat working

--- kong/plugins/jwt-resign/handler.lua
+++ kong/plugins/jwt-resign/handler.lua
@@ -1,6 +1,9 @@
 local kong = kong
 -- https://github.com/cdbattags/lua-resty-jwt
 local jwt = require("resty.jwt")
+-- https://github.com/fffonion/lua-resty-openssl
+local pkey = require("resty.openssl.pkey")
+local json = require("cjson")
 
 local jwt_resign = {
   -- same default priority as openid-connect (enterprise)
@@ -9,26 +12,75 @@ local jwt_resign = {
   VERSION = "1.0.0",
 }
 
+
+local keys = {
+}
+
+local function read_key_from_env(key)
+  local is = keys[key]
+  if is == "err" then
+    return nil
+  end
+  if is == nil then
+    local secret = os.getenv(key)
+    local pkey_priv, err = pkey.new(secret)
+    if err ~= nil then
+      keys[key] = "err"
+      return nil
+    end
+    local jwk_str, err = pkey_priv:tostring("PublicKey", "JWK")
+
+    if err ~= nil then
+      keys[key] = "err"
+      return nil
+    end
+
+    local jwk = json.decode(jwk_str)
+
+    keys[key] = {
+      public_jwk = jwk,
+      private_key = pkey_priv,
+      secret_pem = secret
+    }
+    return keys[key]
+  end
+end
+
 function jwt_resign:access(conf)
-  local opts = {
-    -- The discovery endpoint of the OP. Enable to get the URI of all endpoints (Token, introspection, logout...)
-    discovery = conf.discovery_url,
-    auth_accept_token_as = "header",
-    auth_accept_token_as_header_name = conf.header_name,
+  local key = read_key_from_env(conf.private_key_pem_env_name)
+  if key == nil then
+    kong.response.exit(500, { message = "Cannot fetch the key!" })
+  end
+
+  local headers = kong.request.get_headers()
+  local bearer = headers[conf.header_name]
+
+  local words = {}
+  for w in bearer:gmatch("%S+") do
+    table.insert(words, w)
+  end
+
+  local jwt_obj = jwt:load_jwt(words[2])
+
+  local payload = {
+
   }
+  payload = jwt_obj.payload
+
+  if conf.override_claims then
+    for k, v in pairs(conf.override_claims) do payload[k] = v end
+  end
+
   local jwt_token = jwt:sign(
-                    "lua-resty-jwt",
-                    {
-                        header={typ="JWT", alg="HS256"},
-                        payload={foo="bar"}
-                    }
-                )
-  -- call authenticate for OpenID Connect user authentication
-  -- local res, err = oidc.bearer_jwt_verify(opts)
-  -- if err then
-  --   kong.response.exit(401)
-  -- end
-    kong.response.exit(200, {jwt = jwt_token})
+    key.secret_pem,
+    {
+      header = { typ = "JWT", alg = "RS256", kid = key.public_jwk.kid },
+      payload = payload
+    }
+  )
+  kong.service.request.set_headers({
+    [conf.resigned_header_name] = "Bearer " .. jwt_token
+  })
 end
 
 return jwt_resign
--- kong/plugins/jwt-resign/schema.lua
+++ kong/plugins/jwt-resign/schema.lua
@@ -1,4 +1,4 @@
-local PLUGIN_NAME = "kong-plugin-jwt-oidc-validate"
+local PLUGIN_NAME = "kong-plugin-jwt-resign"
 
 local schema = {
   name = PLUGIN_NAME,
@@ -7,17 +7,31 @@ local schema = {
       config = {
         type = "record",
         fields = {
-          {
-            discovery_url = {
-              type = "string",
-              required = true
-            }
-          },
+          { override_claims = {
+            required = false,
+            type = "map",
+            keys = { type = "string" },
+            values = { type = "string"}
+          } },
           {
             header_name = {
               type = "string",
               required = true,
               default = "authorization"
+            }
+          },
+          {
+            resigned_header_name = {
+              type = "string",
+              required = true,
+              default = "x-gateway-authorization"
+            }
+          },
+          {
+            private_key_pem_env_name = {
+              type = "string",
+              required = true,
+              default = "KONG_PLUGIN_PRIORITY_JWT_RESIGN_PEM_PRIVATE"
             }
           }
         }
