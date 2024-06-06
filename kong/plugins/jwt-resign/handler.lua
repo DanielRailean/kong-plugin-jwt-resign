35677f0d7cc074de8fce70f473414a664d08b26a
DanielRailean <daniel.railean1@gmail.com>
2024-06-06 23:13:13 +0200

    Update handler.lua

--- kong/plugins/jwt-resign/handler.lua
+++ kong/plugins/jwt-resign/handler.lua
@@ -12,75 +12,45 @@ local jwt_resign = {
   VERSION = "1.0.0",
 }
 
-
-local keys = {
-}
-
-local function read_key_from_env(key)
-  local is = keys[key]
-  if is == "err" then
-    return nil
-  end
-  if is == nil then
-    local secret = os.getenv(key)
-    local pkey_priv, err = pkey.new(secret)
-    if err ~= nil then
-      keys[key] = "err"
-      return nil
-    end
-    local jwk_str, err = pkey_priv:tostring("PublicKey", "JWK")
-
-    if err ~= nil then
-      keys[key] = "err"
-      return nil
-    end
-
-    local jwk = json.decode(jwk_str)
-
-    keys[key] = {
-      public_jwk = jwk,
-      private_key = pkey_priv,
-      secret_pem = secret
-    }
-    return keys[key]
-  end
-end
+local secret = os.getenv("KONG_PLUGIN_PRIORITY_JWT_RESIGN_PEM_PRIVATE")
+local key = pkey.new(secret)
+local jwk_str, _ = key:tostring("PublicKey", "JWK")
+local jwk = json.decode(jwk_str)
 
 function jwt_resign:access(conf)
-  local key = read_key_from_env(conf.private_key_pem_env_name)
-  if key == nil then
-    kong.response.exit(500, { message = "Cannot fetch the key!" })
-  end
+
+  local payload = {
+
+  }
 
   local headers = kong.request.get_headers()
   local bearer = headers[conf.header_name]
 
   local words = {}
-  for w in bearer:gmatch("%S+") do
-    table.insert(words, w)
+  for w in bearer:gmatch("%S+")do
+      table.insert(words, w)
   end
 
   local jwt_obj = jwt:load_jwt(words[2])
 
-  local payload = {
-
-  }
   payload = jwt_obj.payload
 
   if conf.override_claims then
-    for k, v in pairs(conf.override_claims) do payload[k] = v end
+    for k,v in pairs(conf.override_claims) do payload[k] = v end
   end
 
   local jwt_token = jwt:sign(
-    key.secret_pem,
+    secret,
     {
-      header = { typ = "JWT", alg = "RS256", kid = key.public_jwk.kid },
+      header = { typ = "JWT", alg = "RS256", kid = jwk.kid },
       payload = payload
     }
   )
   kong.service.request.set_headers({
     [conf.resigned_header_name] = "Bearer " .. jwt_token
   })
+
+
 end
 
 return jwt_resign
